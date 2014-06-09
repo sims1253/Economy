14b512ad049149f93eadfca723e7f739119849cf
sims1253 <m0hawk@gmx.de>
2014-06-09 17:07:22 +0200

    Started trading. Needs more work

--- Source/Participants/MovingTrader.cpp
+++ Source/Participants/MovingTrader.cpp
@@ -1,8 +1,8 @@
-#include "Manufactur.h"
+#include "MovingTrader.h"
 
 
 /**
-\brief Constructor for a Manufactur.
+\brief Constructor for a MovingTrader.
 \param inputProduct product that is consumed and produced.
 \param inputAmount amount that is consumed.
 \param inputCapacity max amount that can be consumed.
@@ -12,35 +12,35 @@
 \param inputMoney starting money.
 */
 //TODO use vectors for items. Also make it possible to have different products in conumption and production
-Manufactur::Manufactur(const int productionAmount, const int productionCapacity, const int consumeAmount, const int consumeMinimum, const int consumeCapacit, const int money, const product item)
-	{
-		this->item = item;
-		this->consumeAmount = consumeAmount;
-		this->consumeMinimum = consumeMinimum;
-		this->consumeCapacity = consumeCapacit;
-		this->money = money;;
-		this->productionAmount = productionAmount;
-		this->productionCapacity = productionCapacity;
-		this->alive = 1;
+MovingTrader::MovingTrader(const int productionAmount, const int productionCapacity, const int consumeAmount, const int consumeMinimum, const int consumeCapacit, const int money, const product item)
+{
+	this->item = item;
+	this->consumeAmount = consumeAmount;
+	this->consumeMinimum = consumeMinimum;
+	this->consumeCapacity = consumeCapacit;
+	this->money = money;;
+	this->productionAmount = productionAmount;
+	this->productionCapacity = productionCapacity;
+	this->alive = 1;
 
-		// TODO initialize vectors
-		this->storageCapacity.push_back(4);
-		this->storageCapacity.push_back(4);
-		this->stock.push_back(1);
-		this->stock.push_back(1);
-	}
+	// TODO initialize vectors
+	this->storageCapacity.push_back(4);
+	this->storageCapacity.push_back(4);
+	this->stock.push_back(1);
+	this->stock.push_back(1);
+}
 
 /*
 \brief deconstructor
 */
-Manufactur::~Manufactur(void)
+MovingTrader::~MovingTrader(void)
 {
 }
 
 /**
 \brief Increases the productionAmount by 1 if productionCapacity is not already reached.
 */
-void Manufactur::increaseProduction(const char change)
+void MovingTrader::increaseProduction(const char change)
 {
 	if (productionAmount<productionCapacity)
 		productionAmount++;
@@ -50,7 +50,7 @@ void Manufactur::increaseProduction(const char change)
 /**
 \brief Reduces the productionAmount by 1. Can't be lower than 0.
 */
-void Manufactur::decreaseProduction(const char change)
+void MovingTrader::decreaseProduction(const char change)
 {
 	if (productionAmount>0)
 		productionAmount--;
@@ -61,7 +61,7 @@ void Manufactur::decreaseProduction(const char change)
 /**
 \brief Increases the consumeAmount by 1 if consumeCapacity is not already reached.
 */
-void Manufactur::increaseConsumption(const char change)
+void MovingTrader::increaseConsumption(const char change)
 {
 	if (consumeAmount<consumeCapacity)
 		consumeAmount++;
@@ -70,14 +70,14 @@ void Manufactur::increaseConsumption(const char change)
 /**
 \brief Reduces the consumeAmount by 1 if consumeMinimum is not already reached.
 */
-void Manufactur::decreaseConsumption(const char change)
+void MovingTrader::decreaseConsumption(const char change)
 {
 	if (consumeAmount>consumeMinimum)
 		consumeAmount++;
 }
 
 /**
-\brief Updates the state of the Manufactur.
+\brief Updates the state of the MovingTrader.
 \param changeProduction change in productivity.
 \param changeConsumption change in Consumption.
 
@@ -88,7 +88,7 @@ Then consumes(destroys) items until storage is empty or consumeAmount is reached
 Then calls consumers::upgrade if >0, consumers::downgrade if <0 or does nothing.
 */
 //TODO nice doc pls
-bool Manufactur::update(char changeProduction, char changeConsumption)
+bool MovingTrader::update(char changeProduction, char changeConsumption)
 {
 	eat();
 	if (alive){
@@ -111,25 +111,31 @@ bool Manufactur::update(char changeProduction, char changeConsumption)
 	}
 	else
 	{
-		std::cout << "This Manufactur is dead!\n";
+		std::cout << "This MovingTrader is dead!\n";
 		return false;
 	}
 }
 
 //just a debug dump of data
-	void Manufactur::dump(int id)
+void MovingTrader::dump(int id)
+{
+
+	char *productNames[] =
 	{
+		"WATER",
+		"FOOD",
+		"TOOLS"
+	};
+	std::cout << "-------------------------------------------- \n";
+	std::cout << "MovingTrader: " << id << std::endl;
+	std::cout << "Produces " << this->productionAmount << " of " << productNames[this->item] << ". Has " << this->money << " moneys." << std::endl;
+	std::cout << "Consumes " << this->consumeAmount << " of " << productNames[this->item] << ". Has " << this->money << " moneys." << std::endl;
+	std::cout << "Storage: \n" << productNames[FOOD] << ": " << this->stock[FOOD] << "/" << this->storageCapacity[FOOD] << std::endl << std::endl;
+}
 
-		char *productNames[] =
-		{
-			"WATER",
-			"FOOD",
-			"TOOLS"
-		};
-		std::cout << "-------------------------------------------- \n";
-		std::cout << "Manufactur: "<< id << std::endl;
-		std::cout << "Produces " << this->productionAmount << " of " << productNames[this->item] << ". Has " << this->money << " moneys." << std::endl;
-		std::cout << "Consumes " << this->consumeAmount << " of " << productNames[this->item] << ". Has " << this->money << " moneys." << std::endl;
-		std::cout << "Storage: \n" << productNames[FOOD] << ": " << this->stock[FOOD] << "/" << this->storageCapacity[FOOD] << std::endl << std::endl;
-	}
-
+//returns how much there is to trade
+//TODO doc pls
+int MovingTrader::tradable(product item)
+{
+	return stock[item];
+}
