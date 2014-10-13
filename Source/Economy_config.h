0ddc820173833e90a0d9f23384668e75ec86e588
sims1253 <m0hawk@gmx.de>
2014-10-14 01:54:08 +0200

    played around some more, still does not work. no idea what I am doing.

---
+++ Source/Economy_config.h
@@ -0,0 +1,12 @@
+
+#ifndef Economy_Economy_CONFIG_H_IN
+#define Economy_Economy_CONFIG_H_IN
+
+#define Economy_VERSION_MAJOR      0
+#define Economy_VERSION_MINOR      0
+#define Economy_VERSION_MICRO      1
+
+#define GIT_BRANCH              
+#define GIT_HEAD                
+
+#endif /* Economy_Economy_CONFIG_H_IN */
